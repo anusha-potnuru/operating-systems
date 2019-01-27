32e80198803511f1bbf16d5a0ba4eefca9cb2ee7
Anusha Potnuru <anusha.sparkx@gmail.com>
2019-01-28 02:06:07 +0530

    final no bugs

--- a2/assign2.c
+++ a2/assign2.c
@@ -12,15 +12,22 @@ extern int errno;
 int main()
 {    
 	while(1)
-	{	
+	{
+
 		char *args[MAX_ARGS];
     	char **next = args;
-        char c[1024];
+        char c[1024],x;
     	int cnt=0;
-    	int j=0,i, bk=0, pipe_process=0;	
+    	int j=0,i=0, bk=0, pipe_process=0;	
     	// taking a line with spaces as input-  
-		// the regex implies to take a charcater sring as input until encountered by a new line
-		scanf("%[^\n]%*c",c);
+		// fgets takes input until newline or EOF
+        printf("$ ");
+
+        fgets(c, 1024, stdin);
+        c[strlen(c)-1] = '\0';
+        if(strlen(c)==0)
+            continue;
+
         char *copy = strdup(c); 
 
 		// exit loop when encountered with quit
@@ -79,6 +86,7 @@ int main()
                     //fprintf(stderr, "file output redirec\n");
                     if(in_redirect==1)
                     {
+                        in=1;
                         fd = open(subtoken,O_RDONLY );
                         if (fd ==-1) 
                         {
@@ -90,6 +98,7 @@ int main()
                     }                    
                     else if(out_redirect==1)
                     {
+                        out=1;
                         // //fprintf(stderr, "file output redirec\n");
                         fd1 = open(subtoken, O_WRONLY| O_CREAT|O_TRUNC,S_IRWXU);
                         if (fd1 ==-1) 
@@ -107,18 +116,16 @@ int main()
                     }
 
                     j = j+ strlen(subtoken); 
-                    //fprintf(stderr, "j,copy1: %d %d\n", j, strlen(copy1));
+                    // fprintf(stderr, "j,copy1: %d %d\n", j, strlen(copy1));
                     while(j<strlen(copy1) && strchr(de,copy1[j])!=NULL)
                     {
                         //fprintf(stderr, "%d\n", j);
                         if(copy1[j]=='<')
                         {
-                            //fprintf(stderr, "in redirect\n");
                             in_redirect=1;
                         }
                         else if(copy1[j]=='>')
                         {
-                            //fprintf(stderr, "out redirect\n");
                             out_redirect=1;
                         }
                         j++;
@@ -136,18 +143,25 @@ int main()
                     {
                         dup2(p[0][1], 1);
                         close(p[0][1]); 
+
+                        close(p[0][0]);
                         
                     }
                     else if(count==pipecount)
                     {                     
                         dup2(p[pipecount-1][0], 0); 
-                        close(p[pipecount-1][0]);                        
+                        close(p[pipecount-1][0]);
+
+                        close(p[pipecount-1][1]);
                     }
                     else
                     {                     
                         dup2(p[count-1][0], 0);
                         dup2(p[count][1], 1); 
-                        close(p[count-1][1]);                                                
+                        close(p[count-1][0]);                                                
+                        close(p[count][1]);
+
+                        close(p[count-1][1]);
                         close(p[count][0]);
                     }
 
@@ -171,19 +185,21 @@ int main()
                     }
                     if(bk)
                     {
-                        waitpid(pid, NULL, WNOHANG);
                         if(count!=pipecount)
                         {                   
                             close(p[count][1]);                         
                         }
                     }                   
                 }
+
                 count++;          
                 token = strtok_r(NULL, "|&", &end_str);
             }
 
-            close(fd);
-            close(fd1);
+            if(in)
+                close(fd);
+            if(out)
+                close(fd1);
             dup2(saved_stdout, 1);
             close(saved_stdout);
             dup2(saved_stdin, 0);
@@ -198,6 +214,7 @@ int main()
             { 
                 if(in_redirect==1)
                 {
+                    in =1;
                     fd = open(token,O_RDONLY );
                     if (fd ==-1) 
                     {
@@ -209,6 +226,7 @@ int main()
                 }                
                 else if(out_redirect==1)
                 {
+                    out = 1;
                     fd1 = open(token, O_WRONLY| O_CREAT|O_TRUNC, S_IRWXU);
                     if (fd1 ==-1) 
                     {
@@ -269,14 +287,24 @@ int main()
             {
                 wait(NULL);
             }
-            close(fd);
-            close(fd1);
+            if(in)
+                close(fd);
+            if(out)
+                close(fd1);
             dup2(saved_stdout, 1);
             close(saved_stdout);
             dup2(saved_stdin, 0);
-            close(saved_stdin);            
+            close(saved_stdin);
+
         }
+
+        dup2(saved_stdout, 1);
+        close(saved_stdout);
+        dup2(saved_stdin, 0);
+        close(saved_stdin); 
         
 	}
+
+
 	return 0;
 }
