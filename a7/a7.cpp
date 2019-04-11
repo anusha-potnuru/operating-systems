9d33042717a6db5befbb49dbce20712341698573
Anusha Potnuru <anusha.sparkx@gmail.com>
2019-04-11 21:26:18 +0530

    add func for free block and fix bugs

--- a7/a7.cpp
+++ a7/a7.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
-
-
+#define SIZE 64
 int file_system_size,block_size;
 int no_of_blocks = file_system_size/block_size;
 
 // block* freeblockptr;
 
+
 typedef struct 
 {
-	char *b_data;
+	int id;
+	char* b_data;
 }block;
 
 typedef struct 
 {
-	block* freeblockptr;
+	vector<block*> *freeblockptr;
 	// block *first_block;
 }superblock;
 
 typedef struct 
 {
 	// block* data[block_size/sizeof(int)];
-	int sptr[block_size/sizeof(int)];
+	int sptr[SIZE/sizeof(int)];
 }singletable;
 
 typedef struct 
 {
-	singletable dptr[block_size/sizeof(int)];
+	singletable dptr[SIZE/sizeof(int)];
 }doubletable;
 
 
@@ -48,22 +50,61 @@ typedef struct
 	char file_name[14];
 }record;
 
-typedef struct 
+typedef struct directory
 {
 	// int inode_no;
-	directory* dot;
-	directory* ddot; 
+	struct directory* dot;
+	struct directory* ddot; 
 	record* r; //14bytes file name
 }directory;
 
-int main()
-{
-	block *file_system = (block*)malloc(sizeof(blocks)*no_of_blocks);
+vector<block*> *freeblockptr;
+superblock *sb;
+vector<block*> &freeptr = *(sb->freeblockptr);
 
-	superblock *sb = (superblock*)malloc(sizeof(superblock));
+block* find_free_block()
+{
+	block* temp;
+	while(1)
+	{
+		if(sb->freeblockptr->empty()==true)
+		{//empty
+			printf("No free blocks\n");
+			return 0;
+		}
+		else
+		{
+			temp = freeptr.front();
+			freeptr.erase(freeptr.begin());
+			return temp;
+		}
+	}
+	// cout<<"free block returned"<<endl;
+	// return free_ptr;
+}
+
+
+void init()
+{
+
+	block *file_system = (block*)malloc(sizeof(block)*no_of_blocks);
+
+	sb = (superblock*)malloc(sizeof(superblock));
 	sb = (superblock*)&file_system[0];
 
 	inode *in = (inode*)malloc(sizeof(inode)*(2*block_size/sizeof(inode)));
+
+	sb->freeblockptr= new vector<block*>;
+	block* temp;
+	for(int i=0; i<no_of_blocks; i++)
+	{
+		sb->freeblockptr->push_back(&file_system[i]);
+	}
+
+}
+int main()
+{
+
 	// how to typecast inode list to two blocks
 
 }
